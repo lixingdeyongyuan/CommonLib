ba30ab6eea88b39c739abb01c01d66f0eee32b4a
lixingdeyongyuan <lixingdeyongyuan@gmail.com>
2020-05-25 11:28:30 +0800

    分割线设置

--- common/src/main/java/com/aixue/common/widget/commonitem/CommonItemBar.kt
+++ common/src/main/java/com/aixue/common/widget/commonitem/CommonItemBar.kt
@@ -9,6 +9,9 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import com.aixue.common.R
 
+/**
+ * NOTE 直接new的话，是不会调onFinishInflate方法的，也就不会添加分割线到view上
+ */
 open class CommonItemBar @JvmOverloads constructor(
     context: Context?,
     attrs: AttributeSet? = null,
@@ -16,32 +19,44 @@ open class CommonItemBar @JvmOverloads constructor(
 ) :
     ConstraintLayout(context, attrs, defStyleAttr) {
 
+    private var mDividerHeight = 2f
+    private var mDividerColor = 0
+
     init {
         attrs?.let {
             var typedArray: TypedArray? = null
             try {
                 typedArray = context!!.obtainStyledAttributes(attrs, R.styleable.CommonItemBar)
-                val dividerHeight =
+                mDividerHeight =
                     typedArray.getDimension(R.styleable.CommonItemBar_dividerHeight, 2f)
-                val dividerColor = typedArray.getColor(
+                mDividerColor = typedArray.getColor(
                     R.styleable.CommonItemBar_dividerColor,
-                    ContextCompat.getColor(context, android.R.color.holo_green_dark)
+                    ContextCompat.getColor(context, R.color.dividerColor)
                 )
                 initView(typedArray)
                 typedArray.recycle()
-                var divider = View(context)
-                var lp = LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    Math.round(dividerHeight)
-                )
-                lp.bottomToBottom = this.bottom
-                divider.layoutParams = lp
-                divider.setBackgroundColor(dividerColor)
-                addView(divider, lp)
             } catch (e: Exception) {
                 typedArray?.recycle()
             }
         }
+    }
+
+    /**
+     * 不放在
+     * @see init
+     * 这个的原因，是因为init方法中没有计算完所有子view的高度就添加了
+     */
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        var divider = View(context)
+        var lp = LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            Math.round(mDividerHeight)
+        )
+        lp.bottomToBottom = this.bottom
+        divider.layoutParams = lp
+        divider.setBackgroundColor(mDividerColor)
+        addView(divider, lp)
     }
 
     open fun initView(typedArray: TypedArray?) {
